61b073b0110a95593fa3fff14ca600728520dee1
faisal-a-n <faisalabdulnazeer@gmail.com>
2022-04-12 03:41:24 +0530

    latest

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # golang-basics
 Repository contains the journey of learning Go Programming language
+
+## Variables
+Variables have two visibility scopes (NO private scope) and they're set by the variable name. When the name starts with an uppercase it is exposed in the package and when lowercase it is local.
+
+### Global Variables
+Global variables need a full declaration explicitly defining the variable type.
+
+### Constants
+Constants can be grouped in a paranthesis. iota is an incrementing counter in a specified constant block. One of the practical implementation is user roles and permissions using bitmasking.
+
+## Arrays and Slices
+Arrays are fixed collections that are passed by value. Slices on the other hand are pointers that point to the underlying array declaration. Using methods __cap()__ and __len()__ to check capacity and length.
+
+### How does slice capacity increment?
+Making some tests show how a 0-size struct will increment capacity by just 1 element, and int or string will duplicate on each growth, and a 3-byte struct "roughly" doubles on each growth. Check in [playground](https://go.dev/play/p/OKtCFskbp2t).
--- src/github.com/faisal-a-n/first-app/main.go
+++ src/github.com/faisal-a-n/first-app/main.go
@@ -1,7 +1,118 @@
 package main
 
-import "fmt"
+// "strconv"
+
+/*
+	VISIBILTIY
+		-Package level variable name starts with an uppercase whereas block level starts with a lowercase
+		-NO private scope
+*/
+// var PackageLevelVariable int = 0
+
+var needsDataType string = "Global variable"
 
 func main() {
-	fmt.Println("Hello World!")
+	/*
+		Variables section
+	*/
+
+	//Declaring variable type explicit
+	// var i int = 42
+	//Declaring variable type implicit (int/float64)
+	// j := 32.33
+
+	//Type casting
+	// i = int(j)
+	// j = float64(i)
+
+	//Converting int to string
+	// text := "Hello World"
+	// text = strconv.Itoa(i)
+
+	//Converting string to int
+	// text := "43"
+	// i = strconv.ParseInt()
+
+	// fmt.Println(i, j, text)
+	/*
+		Variable section ends
+	*/
+
+	// complextNumber := complex(32, 3)
+	// fmt.Printf("%v, %T", complextNumber, complextNumber)
+
+	// runeToByte := 'Z'
+	// c := byte(runeToByte)
+
+	// stringToByte := "Convert this to byte"
+	// b := []byte(stringToByte)
+
+	// fmt.Println(b, string(c))
+	// fmt.Println(runeToByte)
+
+	/*
+		Constants
+	*/
+
+	// const _THIS_IS_A_CONSTANT = "Doesn't change"
+	// fmt.Printf("%T", _THIS_IS_A_CONSTANT)
+
+	//iota auto increments itself in a constant block
+	// const (
+	// 	_SUCCESSFUL = iota + 200
+	// 	_LIMIT_EXCEEDED
+	// 	_TIMEOUT
+	// 	_SESSION_ENDED
+	// )
+
+	// fmt.Println(_SUCCESSFUL)
+	// fmt.Println(_LIMIT_EXCEEDED)
+	// fmt.Println(_TIMEOUT)
+	// fmt.Println(_SESSION_ENDED)
+
+	/*
+		Constants ends
+	*/
+
+	/*
+		Arrays and slices
+	*/
+	//Fixed length
+	// grades := [...]int{1, 2, 3}
+	// grades := [3]int{1, 2, 3}
+
+	// var students []string //Slice
+	// students := [3]string{"Alice", "Bob", "Charlie"}
+
+	//Adding items to array
+	// students = append(students, "Alice", "Bob", "Charlie")
+
+	//Only works on fixed size arrays
+	// studentsCopy := &students //Type is *[3]string
+	// studentsCopy[0] = "New Student"
+
+	/*
+		Slices are reference type data when copied and
+	*/
+	// fmt.Println(students, studentsCopy)
+
+	// a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// b := a[:]   //1 slice of all elements
+	// c := a[3:]  // slice from 4th element to end
+	// d := a[:6]  // slice first 6 elements
+	// e := a[3:6] // slice the 4th, 5th, and 6th elements
+
+	// fmt.Println(a)
+	// fmt.Println(b)
+	// fmt.Println(c)
+	// fmt.Println(d)
+	// fmt.Println(e)
+
+	// a := make([]int, 3, 4)
+	// a = append(a, 1, 2, 3, 4, 5, 6)
+	// fmt.Println(a, len(a), cap(a))
+
+	/*
+		Arrays and slices ends
+	*/
 }
